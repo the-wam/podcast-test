a583f5bdf6c95bdfbda840d61ea0556b52ffec09
titi_lolo <loicbanco@gmail.com>
2024-02-22 11:22:51 +0100

    fix feed.py

--- feed.py
+++ feed.py
@@ -2,7 +2,7 @@ import yaml
 import xml.etree.ElementTree as xml_tree
 
 with open('feed.yaml', 'r') as file:
-    yaml_data = yaml.safr_load(file)
+    yaml_data = yaml.safe_load(file)
 
     rss_element = xml_tree.Element('rss', {'version':'2.0', 
 'xmlns:itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd', 
